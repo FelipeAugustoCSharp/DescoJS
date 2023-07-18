bc1c174aa2969d3864a7428092f083334195e2eb
Felipe Augusto <felipeaugustobonfimdealmeida@gmail.com>
2023-07-18 22:55:31 +0000

    create Main CSS

---
+++ main.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 20;
+
+let total = num1+num2;
+let divisor = num1/num2;
+
+console.log(`total é: ${total}`)
+console.log(`dividido é: ${divisor}`)
+
+
+var a = 7,
+    b = 6,
+    c = 3,
+    d = 4,
+    e = 5;
+
+//var res = b%2 == 0;
+console.log(a>b && b%2 == 0)
