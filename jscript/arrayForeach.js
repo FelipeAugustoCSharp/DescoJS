190fa8c3acc15e97d843d2b424a8deba62793542
Felipe Augusto <felipeaugustobonfimdealmeida@gmail.com>
2023-07-20 22:38:46 +0000

    Aprendendo for/IN & for/OF

--- jscript/arrayForeach.js
+++ jscript/arrayForeach.js
@@ -1,5 +1,21 @@
 var arr = [10,20,30,40,50,60]
 
-arr.forEach(element => {
+/*arr.forEach(element => {
     console.log(element)
-});
+});*/
+
+/*arr.forEach(function (valor, indice, array) {
+    console.log(array[indice])
+})*/
+
+var carro = [{modelo: 'Audi A3', marca: 'Audi', ano: 2020},
+             {modelo: 'Jetta', marca: 'BMW', ano: 2010}
+            ]
+for (let index in carro) {
+    //console.log(carro[index]
+    console.log(carro[index].ano);
+}
+
+for (let veiculo of carro) {
+    console.log(veiculo.marca)
+}
