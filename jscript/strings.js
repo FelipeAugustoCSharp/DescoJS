bec250e1c8d1bc3c4c4180ec4432c55d138f3922
Felipe Augusto <felipeaugustobonfimdealmeida@gmail.com>
2023-07-22 22:14:30 +0000

    trabalhando com length, replace, substring, etc...

--- jscript/strings.js
+++ jscript/strings.js
@@ -31,5 +31,20 @@ console.log(frase.trim());
 var StringNum = "1|2|3|4|5|6|7|8";
 var transformarEmArray = StringNum.split("|");
 
-console.log(transformarEmArray);
+console.log(transformarEmArray[6]);
 
+var posicao1 = nome.indexOf("Felipe");
+console.log(posicao1)
+
+var stringCortada = nome.substring(7,13);
+console.log(stringCortada);
+
+
+var frase = "ola mundo";
+
+if(frase.startsWith("ola")){
+    let sub = frase.substring(4);
+    let novaString = sub.replace("mundo","pessoal");
+    let novaStringUpper = novaString.toUpperCase();
+    console.log(novaStringUpper)
+};
