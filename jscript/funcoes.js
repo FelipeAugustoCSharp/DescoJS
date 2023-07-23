729065ff0d6a837e7ef08a93e890eced834e7c24
Felipe Augusto <felipeaugustobonfimdealmeida@gmail.com>
2023-07-23 00:57:46 +0000

    Co-authored-by: Nayla Harumi Monteiro <Harumi-Monteiro@users.noreply.github.com>
    Subindo att Com Ayla

---
+++ jscript/funcoes.js
@@ -0,0 +1,18 @@
+function multiplicar(x, y){
+    return x * y;
+}
+var a = multiplicar(2,4);
+console.log(a);
+
+var infoCal = function(a,b,c) {
+    return a+b+c;
+}
+console.log(infoCal(1,3,7));
+
+const soma = (num1, num2) =>  num1+num2;
+console.log(soma(10,5))
+
+const numeros = [1,2,3,4,5];
+const valores = numeros.map((num) => num*num);
+console.log(valores)
+//>:C rodou?
