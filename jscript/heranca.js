a5459770a3cc92499d7a8de597ecfa6d6e48660b
Felipe Augusto <felipeaugustobonfimdealmeida@gmail.com>
2023-07-23 18:00:14 +0000

    Herança

---
+++ jscript/heranca.js
@@ -0,0 +1,27 @@
+class DispositivoEletronico{
+    constructor(nome){
+        this.nome = nome;
+        this.status = false; //aqui ele começa Desligado
+    }
+    ligar(){//ESSE if this.status significa basicamente if(this.status == true (verdade). pq qnd ele cria o objeto, ele já vem desligado
+        if (this.status) {
+            console.log("Status = Ok")
+            return;
+        }
+        this.ligado = true;//dai aqui ele liga entao da próxima vez que vc chamar, ele vai entrar no this.status ok
+    }
+}
+//EXTENDS == HERANÇA
+class SmartPhone extends DispositivoEletronico{
+    constructor(nome, cor, modelo){
+        super(nome);
+        this.cor = cor;
+        this.modelo = modelo;
+    }
+}
+
+var s1 = new SmartPhone("Samsung","Preto","A71");
+console.log(s1)
+s1.ligar();
+
+//OK
